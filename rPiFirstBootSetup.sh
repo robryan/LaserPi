8d8577866e8027d9049b1ccadd1907edf33b3c6b
Rob Ryan <robertoryan@gmail.com>
2022-11-14 14:18:44 -0600

    `servoTest.py` works w setup instructions

--- rPiFirstBootSetup.sh
+++ rPiFirstBootSetup.sh
@@ -14,14 +14,18 @@ sudo apt-get update
 sudo apt-get -y install git 
 
 # download LaserPi repo
+rm -rf ./LaserPi/
 git clone https://github.com/robryan/LaserPi.git 
 
 # download Python libs
-sudo apt-get -y install python-pip 
-sudo pip install adafruit-io
-sudo pip install weather-api 
+# sudo apt-get -y install python3-pip 
+# sudo pip install adafruit-io
+# sudo pip install weather-api 
 
 echo -e "\n finished Pi setup, rebooting ...\n"
 
 # restart to make sure it's all available
 sudo reboot
+
+# return control to remote user
+exit 1
