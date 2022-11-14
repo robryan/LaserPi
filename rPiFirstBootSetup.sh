f3681d93ee46eb29a20ed7f22e871973969ccea0
Rob Ryan <robertoryan@gmail.com>
2022-11-14 15:57:43 -0600

    better GPIO servo control lib

--- rPiFirstBootSetup.sh
+++ rPiFirstBootSetup.sh
@@ -3,6 +3,18 @@
 # Set up a newly imaged Raspberry Pi for @robryan/LaserPi ...
 # author: @robryan
 
+echo "
+   .~~.   .~~.
+  '. \ ' ' / .'
+   .~ .~~~..~.
+  : .~.'~'.~. :
+ ~ (   ) (   ) ~
+( : '~'.~.'~' : )
+ ~ .~ (   ) ~. ~
+  (  : '~' :  ) Raspberry Pi
+   '~ .~~~. ~'
+       '~'"
+
 echo -e "starting Raspberry Pi setup ..."
 
 # update device Unix libs
@@ -17,6 +29,10 @@ sudo apt-get -y install git
 rm -rf ./LaserPi/
 git clone https://github.com/robryan/LaserPi.git 
 
+# set the rPI to use smoother "pigpio" servo lib bc "native" sucks
+# https://gpiozero.readthedocs.io/en/stable/api_pins.html#changing-pin-factory
+[ -z "$GPIOZERO_PIN_FACTORY" ] && export GPIOZERO_PIN_FACTORY=pigpio
+
 # download Python libs
 # sudo apt-get -y install python3-pip 
 # sudo pip install adafruit-io
@@ -25,7 +41,5 @@ git clone https://github.com/robryan/LaserPi.git
 echo -e "\n finished Pi setup, rebooting ...\n"
 
 # restart to make sure it's all available
-sudo reboot
-
 # return control to remote user
-exit 1
+sudo reboot && exit 1
