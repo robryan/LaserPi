c6468dd934c8530a3d13b794b4a8f5e9d764a637
Rob Ryan <robertoryan@gmail.com>
2022-11-15 13:39:09 -0600

    native GPIO lib, first test taped to red laser

--- servoTest.py
+++ servoTest.py
@@ -9,19 +9,27 @@ from time import sleep
 
 print("hello world, let's try to move a servo")
 
-servo_1 = Servo(14)     # Servo 1 connected to GPIO port #8
-servo_2 = Servo(15)    # Servo 2 connected to GPIO port #10
+servo_1 = Servo(14)    # Adafruit Servo 1 connected to GPIO port #8
+servo_2 = Servo(15)    # Adafruit Servo 2 connected to GPIO port #10
 
-try:
-    while True:
-        servo_1.min()
-        servo_2.min()
-        sleep(0.5)
-        servo_1.mid()
-        servo_2.mid()
-        sleep(0.5)
-        servo_1.max()
-        servo_2.max()
-        sleep(0.5)
-except KeyboardInterrupt:
-	print("Program stopped")
+"""
+Flex the servos by rotating each to its minimum, midpoint, and max
+"""
+def min_med_max(interval_seconds):
+    try:
+        while True:
+            servo_1.min()
+            servo_2.min()
+            sleep(interval_seconds)
+            servo_1.mid()
+            servo_2.mid()
+            sleep(interval_seconds)
+            servo_1.max()
+            servo_2.max()
+            sleep(interval_seconds)
+    except KeyboardInterrupt:
+        print("Program stopped")
+
+
+
+min_med_max(0.25)
