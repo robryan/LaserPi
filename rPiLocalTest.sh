858b333570d44d577bd63155a68deaf1aa40544b
Rob Ryan <robertoryan@gmail.com>
2022-11-15 14:25:46 -0600

    just `git clone` the repo to get local changes

--- rPiLocalTest.sh
+++ rPiLocalTest.sh
@@ -16,19 +16,17 @@ echo "
 
 echo -e "pulling fresh LaserPi/master..."
 
-# LaserPi repo should already exist, so just `git pull`
-cd ./LaserPi/
-# We don't care about local changes, just set Git config to fast-fwd to master, 
-# because we've edited our changes on our nice Mac and pushed them up to GH
-git config pull.ff only 
-git fetch origin
-git checkout master
-git reset --hard origin/master
-git pull 
+# re-download LaserPi repo
+# yes ... it's inefficient but it also never has weird Git issues
+rm -rf ./LaserPi/
+git clone -q https://github.com/robryan/LaserPi.git
 
 echo -e "Pi now up to date with LaserPi/master ..."
 
 echo -e "running servo test ..."
 
 # servoTest.py has the code we just updated and want to run without opening a new SSH shell
-python servoTest.py
+python LaserPi/servoTest.py
+
+# return control 
+exit 1
